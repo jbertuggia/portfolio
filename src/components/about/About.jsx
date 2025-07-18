d36fc66ead69345a9d8e87dc71fb1a68cfc54179
eleven <juan.bertuggia@gmail.com>
2025-07-18 15:16:40 -0300

    3er commit - correción de texto

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
 
         <div className="textContainer">
           <p>Hola! Soy Juan Marcos Bertuggia, y estoy empezando este camino como desarrollador web.
-            Soy técnico IT hace mas de 10 años y busco crecer ir por otros caminos y seguir creciendo.
+            Soy técnico IT hace mas de 10 años pero ahora busco ir por otros caminos y seguir creciendo.
             Decidí realizar cursos para front-end, empezando por lo básico (HTML, CSS, Javascript) hasta la intruducción a frameworks,en mi caso, a React Js.
             Les dejo mis proyectos que hice durante este tiempo de estudio, en donde van a poder ver las distintas herramientas y conocimientos implementados.
           </p>
